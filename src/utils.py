a8673e539ca3f1ff751ed56211d21aef59d08cf9
Daniel Iong <daniong@umich.edu>
2021-03-26 21:37:18 -0400

    Add apply_storms and associated helper functions

--- src/utils.py
+++ src/utils.py
@@ -36,8 +36,84 @@ def is_numpy(x):
     return isinstance(x, np.ndarray)
 
 
-def get_freq(x):
-    return to_offset(pd.infer_freq(x.index))
+def get_freq_multi_idx(X, time_col=1):
+    def _get_freq_one_storm(x):
+        # Infer freq for one storm
+        times = x.index.get_level_values(time_col)
+        return to_offset(pd.infer_freq(times))
+
+    # Infer frequency within each storm and get unique frequencies
+    freqs = X.groupby(level=0).apply(_get_freq_one_storm).unique()
+
+    # If there is only one unique frequency
+    if len(freqs) == 1:
+        return freqs[0]
+    else:
+        return None
+
+
+def get_freq(X):
+    if isinstance(X.index, pd.MultiIndex):
+        freq = get_freq_multi_idx(X)
+    else:
+        freq = to_offset(pd.infer_freq(X.index))
+
+    return freq
+
+
+def has_storm_index(x):
+
+    if not isinstance(x, (pd.Series, pd.DataFrame)):
+        return False
+
+    if isinstance(x.index, pd.MultiIndex):
+        if "storm" in x.index.names:
+            return True
+
+    return False
+
+
+def _apply_storm(func, x_tuple, y_tuple=None, **kwargs):
+
+    x_storm = x_tuple[0]
+    x = x_tuple[1].droplevel("storm")
+
+    if y_tuple is None:
+        return func(x, **kwargs)
+    else:
+        y_storm = y_tuple[0]
+        assert x_storm == y_storm
+        y = y_tuple[1].droplevel("storm")
+
+        return func(x, y, **kwargs)
+
+
+def apply_storms(func, X, y=None, check=True, **kwargs):
+    # Apply func to each storm and combine results
+
+    # QUESTION: What if only one has MultiIndex?
+    # NOTE: Assuming both or neither have MultiIndex for now
+
+    if check:
+        assert has_storm_index(X) or has_storm_index(
+            y
+        ), "Neither X or y have storm index"
+
+    X_groupby = X.groupby(level="storm")
+    if y is None:
+        apply_iter = (_apply_storm(func, x_tuple, **kwargs) for x_tuple in X_groupby)
+    else:
+        y_groupby = y.groupby(level="storm")
+
+        apply_iter = (
+            _apply_storm(func, x_tuple, y_tuple, **kwargs)
+            for x_tuple, y_tuple in zip(X_groupby, y_groupby)
+        )
+
+    storms = X.index.unique(level="storm")
+    res = pd.concat(apply_iter, keys=storms)
+
+    return res
 
 
 def save_output(obj, path, symlink=False):
@@ -114,6 +190,7 @@ def save_output(obj, path, symlink=False):
         orig_path.symlink_to(output_path)
 
 
+# Might not be needed if using MLFlow
 class Results:
     def __init__(self, model, time, root_dir="~/geomag-forecasting"):
         self.model = model
