687971bf7bd71c66b66b13f1ec82bb1deb875073
Daniel Iong <daniong@umich.edu>
2021-03-31 12:23:57 -0400

    Fix save_output

--- src/utils.py
+++ src/utils.py
@@ -60,7 +60,7 @@ def get_freq(X):
     return freq
 
 
-def save_output(obj, path, symlink=False):
+def save_output(obj, path):
     """Save output object to path.
 
     Parameters
@@ -88,50 +88,25 @@ def save_output(obj, path, symlink=False):
     logger.debug(f"Original working directory is {orig_cwd}")
 
     # Example: /home/danieliong/geomag-forecasting/filename
-    orig_path = Path(to_absolute_path(path))
+    # orig_path = Path(to_absolute_path(path))
 
     # Example: /home/danieliong/geomag-forecasting/outputs/{...}/filename
-    output_path = Path(orig_path.name).resolve()
 
-    # if symlink:
-    #     save_path = output_path
-    # else:
-    #     save_path = orig_path
+    # output_path = Path(orig_path.name).resolve()
+    output_path = Path(path).resolve()
+    output_path.parent.mkdir(parents=True, exist_ok=True)
 
     if is_pandas(obj):
         obj.to_pickle(output_path)
     elif is_numpy(obj):
         np.save(output_path, obj)
-    # elif isinstance(obj, sklearn.pipeline.Pipeline):
-    #     with open(output_path, "wb") as f:
-    #         try:
-    #             joblib.dump(obj, f)
-    #         except:
-    #             dill.dump(obj, f)
-    # try:
-    #     joblib.dump(obj, f)
-    # except (pickle.PickleError, pickle.PicklingError):
-    #     dill.dump(obj, f)
-    #     # FIXME: ModuleNotFound error when unpickling
+
     else:
         with open(output_path, "wb") as f:
             dill.dump(obj, f)
             # pickle.dump(obj, f)
 
-    rel_orig_path = orig_path.relative_to(orig_cwd)
-    rel_output_path = output_path.relative_to(orig_cwd)
-
-    logger.debug(f"Saved output to {rel_output_path}.")
-
-    if symlink:
-        if orig_path.exists() or orig_path.is_symlink():
-            logger.debug(f"Deleting {rel_orig_path} since it already exists...")
-            logger.debug(f"{rel_orig_path} was a symlink to {os.readlink(orig_path)}.")
-
-            orig_path.unlink()
-
-        logger.info(f"Sym-linking {rel_output_path} to {rel_orig_path}...")
-        orig_path.symlink_to(output_path)
+    logger.debug(f"Saved output to {path}.")
 
 
 # Might not be needed if using MLFlow
